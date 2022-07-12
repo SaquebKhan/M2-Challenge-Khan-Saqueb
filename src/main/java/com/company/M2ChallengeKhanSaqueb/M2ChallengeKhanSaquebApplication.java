09404dc5816e2faeffb8acca39ddfa8871222b86
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2022-07-12 00:50:26 -0400

    first commit

---
+++ src/main/java/com/company/M2ChallengeKhanSaqueb/M2ChallengeKhanSaquebApplication.java
@@ -0,0 +1,13 @@
+package com.company.M2ChallengeKhanSaqueb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ChallengeKhanSaquebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2ChallengeKhanSaquebApplication.class, args);
+	}
+
+}
